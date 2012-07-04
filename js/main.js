98f6459ce84a502d4b5be02f3d7c50ee27eeee58
Xalicus <Xalicus@fullsail.edu>
2012-07-04 09:40:31 -0700

    I added some code to my submitData function.

--- js/main.js
+++ js/main.js
@@ -13,8 +13,25 @@ var say = function(message) { console.log(message); };
 var kevPetLib = function() {
 	
 	var submitData = function() {
+		var petType		= document.getElemenetById("petType").value;
+		var petname		= document.getElementById("petName").value;
+		var gender		= document.getElementById("gender").value;
+		var favePet		= document.getElementById("favePet").value;
+		var yearBought	= document.getElementById("yearBought").value;
+		var howMany		= document.getElementById("howMany").value;
 		
+		localStorage.setItem("appPetType", petType);
+		localStorage.setItem("appPetName", );
+		localStorage.setItem("appGender", gender);
+		localStorage.setItem("appFavePet", favePet);
+		localStorage.setItem("appYearBought", yearBought);
+		localStorage.setItem("appHowMany", howMany);
 		return true;
+	};
+	
+	var resetPage = function() {
+		
+		return false;
 	};
 	
 	var getData = function() {
@@ -56,6 +73,7 @@ var kevPetLib = function() {
 	// These are my Public Methods & Properties.
 	return {
 		"submitData" 		: submitData,
+		"resetPage"			: resetPage,
 		"getData" 			: getData,
 		"clearData" 		: clearData,
 		"showArray"			: showArray
@@ -74,6 +92,8 @@ var petLib = new kevPetLib();
 
 // This would be where I check my code in the console.
 say(petLib.submitData());
+
+say(petLib.resetPage());
 
 say(petLib.getData());
 
