007d2d000339abd81f78e1b1e55e3c368bc04a7d
Xalicus <Xalicus@fullsail.edu>
2012-07-03 23:33:20 -0700

    Trying out some js code from the books, and it's not working, that I can see.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ Class: VFW1207
 Name: Project 2
 Date: 07-02-2012
 */
+
+// Say function only if needed.
+var say = function(message) { console.log(message); };
+
+// My Variables
+
+// var = onsubmit ();
+// var = onreset ();
+
+// My Functions Library
+
+var kevPetLib = function() {
+	window.onload = initForm;
+	
+	var initForm = function() {
+		var allTags = document.getElementsByTagName ("*");
+		
+		for (var i=0; i<allTags.length; i++) {
+			if (allTags[i].className.indexOf ("reqd") > -1) {
+				allTags[i].onblur = fieldCheck;
+			}
+		}
+	};
+	
+	var fieldCheck = function() {
+		if (this.value == "") {
+			this.className += " highlight";
+			this.focus();
+	} else {
+		this.className = "reqd";
+		}
+	};
+};
+
+// My Other code
+
