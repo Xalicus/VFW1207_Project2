01948b2ef549213916d22ab39a25ea72af59e787
Xalicus <Xalicus@fullsail.edu>
2012-07-05 08:41:01 -0700

    I'm running out of time with these videos, and I have work all day today, so I'm just going to turn in what I have.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 Author: Kevin Ward
 Class: VFW1207
 Name: Project 2
-Date: 07-02-2012
+Date: 07-05-2012
 */
 
 // Say function only if needed.
 var say = function(message) { console.log(message); };
 
-// My Functions Library
+// My Functions
 
 window.addEventListener("DOMContentLoaded", function(){
 	
@@ -45,6 +45,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		};
 	};
 	
+	// Finds the value of the Checkbox
 	function getCheckboxValue(){
 		if ($("favePet").checked) {
 			faveValue = $("favePet").value;
@@ -53,6 +54,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		};
 	};
 	
+	// My submitData function
 	var submitData = function() {
 		var id				= Math.floor(Math.random()*1000001);
 		// Gather round ye olde form field values, and store in ye olde objects.
@@ -64,8 +66,8 @@ window.addEventListener("DOMContentLoaded", function(){
 		var item			= {};
 			item.petType	= ["Pet Type:", $("petType").value];
 			item.petName	= ["Pet Name:", $("petName").value];
-//			item.gender		= ["Gender:", genderValue];
-//			item.favepet	= ["Favorite Pet:", faveValue];
+			item.gender		= ["Gender:", genderValue];
+			item.favepet	= ["Favorite Pet:", faveValue];
 			item.yearBought	= ["Year Bought:", $("yearBought").value];
 			item.koolness	= ["Koolness:", $("koolness").value];
 			item.comments	= ["Comments:", $("comments").value];
@@ -74,7 +76,8 @@ window.addEventListener("DOMContentLoaded", function(){
 		alert("Pet is saved to the Pokedex!")
 	};
 	
-	var getData() {
+	// My getData function
+	var getData = function() {
 		// This is supposed to write data from Local Storage back to the browser.
 		var makeDiv = document.createElement("div");
 		makeDiv.setAttribute("id", "items");
@@ -99,94 +102,6 @@ window.addEventListener("DOMContentLoaded", function(){
 		};
 	};
 	
-	
-	
-	
-	
-	
-	
-	var petGroups = ["--Choose A Pet Type--", "Dogs", "Cats", "Rodents", "Birds", "Farm Animals"],
-		sexvalue,
-		faveValue = "No"
-	;
-	makeCats();
-	
-	var showData = $("showData");
-	showData.addEventListener("click", getData);
-//	var clearData = $("clearData");	
-//	clearData.addEventListener("click", clearData);
-	var save = $("submitData");
-	save.addEventListener("click", submitData);
-	
-	
-});
-/*
-var kevPetLib = function() {
-	
-	// My Submit Data Function, with random number key:value pairs.
-	var submitData = function(key) {
-		getSelectedRadio();
-		setCheckboxValue();
-		var petType		= document.getElementById("petType").value;
-		var petname		= document.getElementById("petName").value;
-		var gender		= document.getElementById("gender").value;
-		var favePet		= document.getElementById("favePet").value;
-		var yearBought	= document.getElementById("yearBought").value;
-		var koolness	= document.getElementById("koolness").value;
-		var comments	= document.getElementById("comments").value;
-		
-		localStorage.setItem("appPetType", $("petType").value);
-		localStorage.setItem("appPetName", $("petName").value);
-		localStorage.setItem("appGender", $("gender").value);
-		localStorage.setItem("appFavePet", $("favePet").value);
-		localStorage.setItem("appYearBought", $("yearBought").value);
-		localStorage.setItem("appKoolness", $("koolness").value);
-		localStorage.setItem("appComments", $("comments").value);
-		
-		alert("Pet Saved!");
-	};
-	
-	// My Get Data Function
-	var getData = function() {
-		// hide the form and display the data from the localStorage.
-		
-		if (localStorage.getItem("appPetType")) {
-			var petType = localStorage.getItem("appPetType");
-			var petName = localStorage.getItem("appPetName");
-			var gender = localStorage.getItem("appGender");
-			var favePet = localStorage.getItem("appFavePet");
-			var yearBought = localStorage.getItem("appYearBought");
-			var koolness = localStorage.getItem("appKoolness");
-			var comments = localStorage.getItem("appComments");
-				var glist = document.getElementById("glist");
-				while (glist.firstChild)
-					glist.removeChild(glist.firstChild);
-				var newUl = document.createElement("ul");
-				var newLi = document.createElement("li");
-				newUl.appendChild(newLi);
-				var liTxt = document.createTextNode(petType);
-				var liTxt = document.createTextNode(petName);
-				var liTxt = document.createTextNode(gender);
-				var liTxt = document.createTextNode(favePet);
-				var liTxt = document.createTextNode(yearBought);
-				var liTxt = document.createTextNode(koolness);
-				var liTxt = document.createTextNode(comments);
-				newLi.appendChild(liTxt);
-				glist.appendChild(newUl);
-		} else {
-			var petName = "Enter a Name for your Kool Pet!";
-		}
-		document.getElementById("petType").value = petType;
-		document.getElementById("petName").value = petName;
-		document.getElementById("gender").value = gender;
-		document.getElementById("favePet").value = favePet;
-		document.getElementById("yearBought").value = yearBought;
-		document.getElementById("koolness").value = koolness;
-		document.getElementById("comments").value = comments;
-		
-		return true;
-	};
-	
 	// My Clear Data Function
 	var clearData = function() {
 		clear; localStorage.clear();
@@ -198,34 +113,20 @@ var kevPetLib = function() {
 		// Supposed to show something here.
 		return true;
 	};
-*/
-/*
-	// These are my Public Methods & Properties.
-	return {
-		"submitData" 		: submitData,
-		"getData" 			: getData,
-		"clearData" 		: clearData,
-		"showArray"			: showArray
-	};
 	
-};
+	var petGroups = ["--Choose A Pet Type--", "Dogs", "Cats", "Rodents", "Birds", "Farm Animals"],
+		sexvalue,
+		faveValue = "No"
+	;
+	makeCats();
+	
+	var save = $("submitData");
+	save.addEventListener("click", submitData);
+	var showData = $("getData");
+	showData.addEventListener("click", getData);
+	var clearData = $("clearData");	
+	clearData.addEventListener("click", clearData);
+	
+	
+});
 
-// My Variables
-
-var petLib = new kevPetLib();
-
-// My Other code
-
-
-
-
-
-// This would be where I check my code in the console.
-say(submitData());
-
-say(getData());
-
-say(clearData());
-
-say(showArray());
-*/
